1eebd85e61bf41c6f313013796702ba1f143732c
muling <gujiaho321@126.com>
2022-04-01 14:39:45 +0800

    add:初始化项目

---
+++ pages/center/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view >
+ 个人中心
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view >
+   首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
